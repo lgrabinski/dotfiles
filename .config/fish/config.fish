7f26482d6bd0ad96fc293fba23d70b683b48fa12
lgrabinski <lgrabinski@gmail.com>
2021-07-20 13:14:32 +0200

    add: config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+# PATHs
+set -U fish_user_paths $HOME/.local/bin
+
+# VARIABLES
+set -gx TERMINAL "st"
+set -gx EDITOR "nvim"
+
+setxkbmap pl
+
+# Welcome messages
+set fish_greeting  	# Suppresses fish's welcome message
+pfetch
+
+# Aliases
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias ll='exa -l'
+alias la='exa -la'
+
+# Promt
+starship init fish | source
